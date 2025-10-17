2b95639508fb56eda3280d9fbb00f5bf37dbe3ec
Tomi <thomas.bartes2@gmail.com>
2025-10-17 10:13:12 +0200

    Yahoo extraction / Works

--- Phase 1/btc_ohlcv.py
+++ Phase 1/btc_ohlcv.py
@@ -1,4 +1,4 @@
 import yfinance as yf
 btcusd  = yf.Ticker("BTC-USD")
-data = btcusd.history(interval="1m", period="max")
+data = btcusd.history(interval="1h", period="max")
 print(data.to_string())
