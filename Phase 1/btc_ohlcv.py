984d1ad06622afa07a843fa063e15717d73a0450
Tomi <thomas.bartes2@gmail.com>
2025-10-24 09:08:28 +0200

    test aktion für Verbindung herstellung

--- Phase 1/btc_ohlcv.py
+++ Phase 1/btc_ohlcv.py
@@ -10,34 +10,64 @@ try:
         port=3306,
         user="root",
         password="Digimon@4123",
+        database="yahoo_finance_extraction"
     )
 
     if connection.is_connected():
         print("Connected to MySQL")
 
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE test (id int(3), text varchar(255), datetime datetime)")
-    cursor.close()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS EX_NIHILO (
+            quote_date DATE PRIMARY KEY,
+            open DECIMAL (18,8),
+            high DECIMAL (18,8),
+            low DECIMAL (18,8),
+            close DECIMAL (18,8),
+            volume BIGINT,
+        )
+        """
+    )
 
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO test (id, text, datetime) VALUES (%s, %s, %s)")
-    cursor.close()
+    btcusd = yf.Ticker("BTC-USD")
+    data = btcusd.history(period="max", interval="1d")
+
+    insert_stm = """
+        INSERT INTO EX_NIHILO (quote_date, open, high, low, close, volume)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE
+            open=VALUES(open),
+            high=VALUES(high),
+            low=VALUES(low),
+            close=VALUES(close),
+            volume=VALUES(volume)
+        """
+
+    rows = [
+        (
+            idx.date(),
+            float(row["Open"]),
+            float(row["High"]),
+            float(row["Low"]),
+            float(row["Close"]),
+            float(row["Volume"])
+        )
+        for idx, row in data.iterrows()
+    ]
+
+    cursor.executemany(insert_stm, rows)
     connection.commit()
+    print(f"{cursor.rowcount} rows inserted.")
 
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM test")
-    result = cursor.fetchall()
-    cursor.close()
-    for data in result:
-        print("Nummer: " + str(data[0]) + "; Text: " + (data[1]))
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
+finally:
+    if cursor:
+        cursor.close()
+    if connection and connection.is_connected():
+        connection.close()
 
 # Data Extraction from Yahoo Finance
 #btcusd  = yf.Ticker("BTC-USD")
 #data = btcusd.history(interval="1d", period="max")
 #print(data.to_string())
 
-
-connection.close()
